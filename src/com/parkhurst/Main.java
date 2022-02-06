9823f1d06fd3123b9f543a075f101f33e51cc87a
John Parkhurst <jparkhurst120@gmail.com>
2022-02-05 23:11:59 -0500

    Initial files

---
+++ src/com/parkhurst/Main.java
@@ -0,0 +1,26 @@
+package com.parkhurst;
+//This is My Temperature Converter
+public class Main {
+
+    /**
+     * @param temp The temperature in Celcius
+     * @return the temperature in Fahrenheit as a float
+     */
+    public float cToF(float temp){
+
+        return temp;
+    }
+
+    /**
+     * @param temp The temperature in fahrenheit
+     * @return the temperature in Celsius as a float
+     */
+    public float fToC(float temp){
+
+        return temp;
+    }
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(5);
+    }
+}
