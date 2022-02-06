22df062e4b228badacaacb92079ea8cd86b25b36
John Parkhurst <jparkhurst120@gmail.com>
2022-02-06 01:01:22 -0500

    Java Files
    Basic Temperature Converator Complete!
    Setting up Junit tests in morning

--- src/com/parkhurst/Main.java
+++ src/com/parkhurst/Main.java
@@ -6,8 +6,8 @@ public class Main {
      * @param temp The temperature in Celcius
      * @return the temperature in Fahrenheit as a float
      */
-    public float cToF(float temp){
-
+    public static float cToF(float temp){
+        temp = (temp*9/5) +32;
         return temp;
     }
 
@@ -15,12 +15,14 @@ public class Main {
      * @param temp The temperature in fahrenheit
      * @return the temperature in Celsius as a float
      */
-    public float fToC(float temp){
-
+    public static float fToC(float temp){
+        temp = (temp-32) * 5/9;
         return temp;
     }
     public static void main(String[] args) {
-	// write your code here
-        System.out.println(5);
+        // write your code here
+        float myTemp = cToF(43);
+        assert myTemp != 109.4 : " Wrong!";
+        System.out.println(cToF(43));
     }
 }
